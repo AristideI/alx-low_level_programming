7137b907a7fb06ff1348607d0ddcfa11b2f2e9fb
root <aristidewunsler@gmail.com>
2022-12-09 07:47:20 -0800

    g

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	puts("\"progrsmming is like building a multilingual puzzle");
+	return (0);
+
+}
